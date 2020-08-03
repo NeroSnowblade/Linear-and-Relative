49a79cb44cc3572c4da67f8867bcb8ee812717bb
NeroSnowblade <fajarmustaqim11@gmail.com>
2020-08-03 23:02:16 +0700

    First Update? :v
    Main + Secondary XML

--- app/src/main/java/com/fajarmush/linearrelativelayout/MainActivity.java
+++ app/src/main/java/com/fajarmush/linearrelativelayout/MainActivity.java
@@ -3,12 +3,21 @@ package com.fajarmush.linearrelativelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void moveRelative(View view) {
+        setContentView(R.layout.relative_page);
+    }
+
+    public void moveMain(View view) {
         setContentView(R.layout.activity_main);
     }
 }
